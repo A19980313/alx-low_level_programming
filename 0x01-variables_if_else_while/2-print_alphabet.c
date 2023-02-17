1ff3f8e16e5671d3c6fea861403ee024f68b1ac4
A19980313 <abdulazeezamina98@gmail.com>
2023-02-17 12:30:24 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lower case
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++);
+	{
+		putchar(c);
+	}
+	putchar("\n");
+	return (0);
+}
