8a4905e497b60d2f4a28d852f0d1366686cc216e
A19980313 <abdulazeezamina98@gmail.com>
2023-02-18 08:12:22 -0800

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main -prints the alphabet in lowercase 
- * followed by a new line, except q and e
- * Return: Always 0 (Success)
- */
+* main -prints the alphabet in lowercase
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 
 {
 	char la;
 
-	for (la = 'a'; la <= 'z'; la++);
+	for (la = 'a'; la <= 'z'; la++)
 	{
-		if (la != 'e' && la != 'q')
-			putchar(la);
+	if (la != 'e' && la != 'q')
+		putchar(la);
 	}
-	
 	putchar('\n');
 	return (0);
 }
