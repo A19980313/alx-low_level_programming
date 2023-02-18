343602a83f65d1a3a54502c4082920bbce41d405
A19980313 <abdulazeezamina98@gmail.com>
2023-02-18 09:10:09 -0800

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the number in base 16
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+	putchar('\n');
+	return (0);
+}
