9beefc0289f418672821915b14b20decdaa61b91
A19980313 <abdulazeezamina98@gmail.com>
2023-02-18 07:28:55 -0800

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,8 +15,8 @@ int main(void)
 
 	for (la = 'a'; la <= 'z'; la++);
 	{
-		if (la != 'e' && la != 'q')
-			putchar(la);
+	if (la != 'e' && la != 'q')
+	putchar(la);
 	}
 	putchar('\n');
 	return (0);
