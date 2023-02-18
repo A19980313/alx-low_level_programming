52057a8b3ab5121fa449b06f93cc9f62ca5ce97d
A19980313 <abdulazeezamina98@gmail.com>
2023-02-18 09:52:12 -0800

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d, p;
+
+	for (d = '0'; d < 'q'; d++)
+	{
+	for (p = d + 'i'; p <= 'q'; p++)
+	{
+	if (p != d)
+	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == 'q')
+	continue;
+	putchar(',');
+	putchar(' ')
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
