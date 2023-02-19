bb54edc049dae391d9ebfc561d73cafc1fbbec3c
A19980313 <abdulazeezamina98@gmail.com>
2023-02-19 09:32:21 -0800

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,6 +10,7 @@ int main(void)
 
 {
 	int p, q;
+
 	for (p = 0; p <= 98; p++)
 	{
 	for (q = p + 1; q <= 99; q++)
@@ -20,12 +21,12 @@ int main(void)
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
 	if (p == 98 && q == 99)
-	continue ;
+	continue;
 	putchar(',');
 	putchar(' ');
 	}
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 
 }
