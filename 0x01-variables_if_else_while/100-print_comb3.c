29f703a5ee3e0b6bbdaaea1894a0a96d5354e700
A19980313 <abdulazeezamina98@gmail.com>
2023-02-20 07:13:31 -0800

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,6 +9,7 @@ int main(void)
 
 	for (d = '0'; d < '9'; d++)
 	{
+
 	for (p = d + '1'; p <= '9'; p++)
 	{
 	if (p != d)
@@ -17,6 +18,7 @@ int main(void)
 	putchar(p);
 	if (d == '8' && p == '9')
 	continue;
+
 	putchar(',');
 	putchar(' ');
 	}
