306869a5ca35eb7568be861383c582b1007d1213
A19980313 <abdulazeezamina98@gmail.com>
2023-02-23 01:34:42 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,7 @@ void print_times_table(int n)
 				}
 				else if (k >= 100)
 				{
-					_putchar(',');
+					_putchar(',')
 					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
