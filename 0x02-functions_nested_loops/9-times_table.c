4b83d2d43da5fdd0d42c26ce0d21d5d231b6f946
A19980313 <abdulazeezamina98@gmail.com>
2023-02-22 21:56:36 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			k = j + 1;
+			k = j * 1;
 			if (j == 0)
 			{
 				_putchar(k + '0');
@@ -19,12 +19,17 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-				_putchar('\n');
-				j++;
+				_putchar(' ');
+				_putchar(k + '0');
 			}
-			i++;
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
