8e4acec2041ec9f16ffa2adea378ef209a872d95
A19980313 <abdulazeezamina98@gmail.com>
2023-02-23 01:41:41 -0800

    100times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,27 +20,27 @@ void print_times_table(int n)
 				}
 				else if (k < 10 && j != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 				}
 				else if (k >= 100)
 				{
-					_putchar(',')
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+				_putchar(',')
+				_putchar(' ');
+				_putchar((k / 100) + '0');
+				_putchar(((k / 10) % 10) + '0');
+				_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
